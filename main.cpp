f705c5851f409fef229b2214805fa98b5d3c36cd
Joshua Akangah <akangah89@hmail.com>
2020-04-23 07:04:01 +0000

    removed uneccessary functions

--- main.cpp
+++ main.cpp
@@ -35,13 +35,6 @@ bool addToBoard(char arr[], char item, int position){
 	}
 }
 
-void checkForWin(char arr[]){
-	char temp[] = {};
-	for (int i=0;i<sizeof(arr);++i){
-		temp[i] = arr[i];
-	}
-}
-
 bool allHorizontal(char arr[]){
 	return (arr[0] == arr[1] == arr[2] || arr[3] == arr[4] == arr[5] || arr[6] == arr[7] == arr[8]);
 }
